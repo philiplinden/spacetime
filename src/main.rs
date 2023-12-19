f86eb9584287ba68d675794cc566274c846ed107
Philip Linden <lindenphilipj@gmail.com>
2023-12-18 20:31:47 -0500

    fix broken placeholder code

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ pub static TOROIDAL: bool = true;
 pub static DISCRETIZATION: f32 = 1.0;
 
 fn main() {
-    let step = 100;
-    let state = Realm::new((1_000_000.0, 1_000_000.0));
+    // let step = 100;
+    // let state = Realm::new((1_000_000.0, 1_000_000.0));
 
-    simulate!(state, step, 10);
+    // simulate!(state, step, 10);
 }
