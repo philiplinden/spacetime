ccc9f07c7cc2a6a969c3c6c3046a7d8b19f223b7
philiplinden <lindenphilipj@gmail.com>
2024-01-01 11:58:04 -0500

    docstrings on time dilation

--- src/spacetime.rs
+++ src/spacetime.rs
@@ -1,3 +1,24 @@
 /*!
  * This module handles the warping of spacetime. This includes gravity and time dilation.
+ * 
+ * The time dilation of an accelerating object is given by
+ *
+ *      tau(t) = (c / g) arcsinh(g * t / c), where
+ *
+ *          t -> elapsed proper time
+ *          c -> speed of light
+ *          g -> acceleration
+ * 
+ * For other relativistic effects, we may need to reference the Lorentz factor given by
+ *
+ *      gamma = 1 / sqrt(1 - v^2 / c^2), where
+ *
+ *          v -> speed of moving observer
+ *          c -> speed of light
+ * 
+ * Time dilation can also be expressed in terms of Lorentz factor:
+ * 
+ *      dt = gamma * dtau
+ * 
+ * Note that if we integrate over time and substitute v -> (g * t), we arrive at the same definition as before.
  */
