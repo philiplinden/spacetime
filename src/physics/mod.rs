a384f8c6e31360a6a2a50e669771833628241d7b
Philip Linden <lindenphilipj@gmail.com>
2024-08-07 00:46:11 -0400

    chatgpt helps me out

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,8 +1,13 @@
+mod time_dilation;
+
 use avian2d::prelude::*;
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(PhysicsPlugins::default().with_length_unit(1.0));
+    app.add_plugins((
+        PhysicsPlugins::default().with_length_unit(1.0),
+        time_dilation::plugin,
+    ));
 
     // we will handle gravity ourselves
     app.insert_resource(Gravity(Vec2::ZERO.into()));
