e8c9044964848a907d54b661eaca37687410de25
Philip Linden <lindenphilipj@gmail.com>
2024-08-13 01:45:10 -0400

    ui: datepicker works!

--- spacetime/src/ui/datepicker.rs
+++
@@ -1,5 +0,0 @@
-use bevy::prelude::*;
-
-pub(super) fn plugin(app: &mut App) {
-    // app.add_plugins();
-}
--- spacetime/src/ui/shell.rs
+++ spacetime/src/ui/shell.rs
@@ -16,28 +16,12 @@ fn update_shell(
     egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
         egui::menu::bar(ui, |ui| {
             ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
-                set_time_menu(ui, &mut coordinate_time);
+                super::datetime::set_time_menu(ui, &mut coordinate_time);
             });
             ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
                 egui::warn_if_debug_build(ui);
                 ui.label(format!("Coordinate Time: {:?}", coordinate_time.as_ref()));
             });
-        });
-    });
-}
-
-
-fn set_time_menu(ui: &mut egui::Ui, coordinate_time: &mut ResMut<CoordinateTime>) {
-    ui.menu_button("Coordinate Time...", |ui| {
-        ui.menu_button("Scale...", |ui| {
-            ui.vertical(|ui| {
-                ui.radio_value(&mut coordinate_time.scale, TimeScale::UTC, "UTC");
-                ui.radio_value(&mut coordinate_time.scale, TimeScale::TAI, "TAI");
-                ui.radio_value(&mut coordinate_time.scale, TimeScale::BDT, "BDT");
-            });
-        });
-        ui.menu_button("Epoch...", |ui| {
-            // ui.add(egui_extras::DatePickerButton::new(&mut coordinate_time.start_epoch));
         });
     });
 }
