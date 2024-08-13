594d1b1738d472166441720ea750d39ffcf98584
Philip Linden <lindenphilipj@gmail.com>
2024-08-12 23:49:56 -0400

    time: lofitime crate is feature complete

--- lofitime/src/lib.rs
+++ lofitime/src/lib.rs
@@ -27,13 +27,47 @@ pub trait HifiDuration {
     fn to_lofi_duration(&self) -> chrono::Duration;
 }
 
+impl HifiDuration for hifitime::Duration {
+    fn to_lofi_duration(&self) -> chrono::Duration {
+        let (centuries, nanos) = self.to_parts();
+        let centuries_as_days = centuries as i64 / hifitime::DAYS_PER_CENTURY_I64;
+        let chrono_days = chrono::Duration::days(centuries_as_days);
+        let chrono_nanos = chrono::Duration::nanoseconds(nanos as i64);
+        chrono_days + chrono_nanos
+    }
+}
+
 /// Adds functions to a chrono time so it can be represented as a hifitime epoch.
 pub trait LofiDateTime {
-    fn to_hifi_epoch(&self, timescale: hifitime::TimeScale) -> hifitime::Epoch;
-    fn to_hifi_utc(&self) -> hifitime::Epoch;
+    fn to_hifi_epoch(&self) -> Result<hifitime::Epoch, hifitime::Errors>;
+}
+
+impl<Tz> LofiDateTime for chrono::DateTime<Tz>
+where
+    Tz: chrono::TimeZone,
+{
+    fn to_hifi_epoch(&self) -> Result<hifitime::Epoch, hifitime::Errors> {
+        if let Some(utc_nanos) = self.to_utc().timestamp_nanos_opt() {
+            Ok(hifitime::Epoch::from_utc_duration(
+                hifitime::Duration::from_truncated_nanoseconds(utc_nanos),
+            ))
+        } else {
+            Err(hifitime::Errors::Overflow)
+        }
+    }
 }
 
 /// Adds functions to a chrono duration so it can be represented as a hifitime duration.
 pub trait LofiDuration {
-    fn to_hifi_duration(&self) -> hifitime::Duration;
+    fn to_hifi_duration(&self) -> Result<hifitime::Duration, hifitime::Errors>;
+}
+
+impl LofiDuration for chrono::Duration {
+    fn to_hifi_duration(&self) -> Result<hifitime::Duration, hifitime::Errors> {
+        if let Some(nanos) = self.num_nanoseconds() {
+            Ok(hifitime::Duration::from_truncated_nanoseconds(nanos))
+        } else {
+            Err(hifitime::Errors::Overflow)
+        }
+    }
 }
